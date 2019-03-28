9aa96ce890e012dc2348f293d3972f5bae50a02e
Jaime Diaz <jdsolutions817@gmail.com>
2019-03-27 20:56:41 -0500

    Static files, frontend single project

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Jaime Díaz | Página personal</title>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1, minimum-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <!-- BOOTSTRAP   LIB -->
+        <link rel="stylesheet" href="css/bs/bootstrap.min.css">
+
+        <!-- FONTAWESOME CDN -->
+        <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">-->
+
+        <!-- FONTAWESOME LIB -->
+        <link rel="stylesheet" href="css/fawesome/all.min.css">
+
+        <!-- CUSTOM  STYLES -->
+        <link rel="stylesheet" href="css/styles.css">
+
+    </head>
+
+    <body>
+        <section class="bg-primary text-secundary container">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi ipsum exercitationem asperiores, dignissimos dolor animi quisquam nulla praesentium corrupti illo quaerat magni. Molestiae assumenda temporibus facere odio perspiciatis minus repudiandae?
+            <i class="fas fa-address-card"></i>
+        </section>
+
+
+
+        <!-- *********************************************************************** -->
+        <!-- JQUERY    LIB -->
+        <script src="js/jquery-plugins/jquery-3.2.1.min.js"></script>
+
+        <!-- POPPER    CDN -->
+        <!--https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js-->
+
+        <!-- POPPER    LIB -->
+        <script src="js/jquery-plugins/popper.min.js"></script>
+
+        <!-- BOOTSTRAP LIB -->
+        <script src="js/bs/bootstrap.min.js"></script>
+    </body>
+</html>
